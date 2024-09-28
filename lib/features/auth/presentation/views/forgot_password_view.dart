e011760eff1e81d916004c7d9fa631e3d74715b6
EslamElnady0 <eslamelnady254@gmail.com>
2024-09-28 10:10:44 +0300

    provide cubits for the screens

--- lib/features/auth/presentation/views/forgot_password_view.dart
+++ lib/features/auth/presentation/views/forgot_password_view.dart
@@ -1,7 +1,9 @@
+import 'package:exo_planets/core/DI/dependency_injection.dart';
 import 'package:exo_planets/core/helpers/app_assets.dart';
 import 'package:exo_planets/core/widgets/custom_scaffold.dart';
+import 'package:exo_planets/features/auth/presentation/view%20models/send%20password%20reset%20cubit/send_password_reset_cubit.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'widgets/forgot_password_view_body.dart';
 
 class ForgotPasswordView extends StatelessWidget {
@@ -9,9 +11,12 @@ class ForgotPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScaffold(
+    return CustomScaffold(
       backgroundImage: AppAssets.signInUpBackground,
-      body: ForgotPasswordViewBody(),
+      body: BlocProvider(
+        create: (context) => getIt<SendPasswordResetCubit>(),
+        child: const ForgotPasswordViewBody(),
+      ),
     );
   }
 }
