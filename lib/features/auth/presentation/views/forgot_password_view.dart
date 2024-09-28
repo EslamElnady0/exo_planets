375b0ded2417b4598aa5d7d0dd579b974065f24e
EslamElnady0 <eslamelnady254@gmail.com>
2024-09-28 10:49:02 +0300

    finished forgot password states

--- lib/features/auth/presentation/views/forgot_password_view.dart
+++ lib/features/auth/presentation/views/forgot_password_view.dart
@@ -1,9 +1,6 @@
-import 'package:exo_planets/core/DI/dependency_injection.dart';
 import 'package:exo_planets/core/helpers/app_assets.dart';
 import 'package:exo_planets/core/widgets/custom_scaffold.dart';
-import 'package:exo_planets/features/auth/presentation/view%20models/send%20password%20reset%20cubit/send_password_reset_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'widgets/forgot_password_view_body.dart';
 
 class ForgotPasswordView extends StatelessWidget {
@@ -11,12 +8,9 @@ class ForgotPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
+    return const CustomScaffold(
       backgroundImage: AppAssets.signInUpBackground,
-      body: BlocProvider(
-        create: (context) => getIt<SendPasswordResetCubit>(),
-        child: const ForgotPasswordViewBody(),
-      ),
+      body: ForgotPasswordViewBody(),
     );
   }
 }
--- lib/features/auth/presentation/views/widgets/forgot_password_view_body.dart
+++ lib/features/auth/presentation/views/widgets/forgot_password_view_body.dart
@@ -1,11 +1,15 @@
+import 'package:exo_planets/core/DI/dependency_injection.dart';
 import 'package:exo_planets/core/helpers/spacing.dart';
 import 'package:exo_planets/core/widgets/custom_button.dart';
 import 'package:exo_planets/features/auth/presentation/views/widgets/auth_header.dart';
 import 'package:exo_planets/features/auth/presentation/views/widgets/custom_auth_text_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../../core/helpers/show_toast.dart';
 import '../../../../../core/theme/app_text_styles.dart';
+import '../../view models/send password reset cubit/send_password_reset_cubit.dart';
 import 'bottom_screen_actions.dart';
 
 class ForgotPasswordViewBody extends StatefulWidget {
@@ -33,36 +37,60 @@ class _ForgotPasswordViewBodyState extends State<ForgotPasswordViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 20.w),
-      child: Form(
-        key: formKey,
-        child: Column(
-          children: [
-            const AuthHeader(),
-            Text("Enter your email address to reset your password",
-                style: AppTextStyles.font12WhiteW600),
-            vGap(170),
-            CustomAuthTextFormField(
-                hintText: "Email",
-                suffixIcon: Icons.email_outlined,
-                controller: emailController),
-            vGap(20),
-            CustomButton(
-                text: "Send",
-                onTap: () {
-                  if (formKey.currentState!.validate()) {}
-                }),
-            const Spacer(),
-            vGap(40),
-            const BottomScreenActions(
-              backExists: true,
-              skipExists: false,
+    return BlocProvider(
+      create: (context) => getIt<SendPasswordResetCubit>(),
+      child: Builder(builder: (context) {
+        return Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Form(
+            key: formKey,
+            child: Column(
+              children: [
+                const AuthHeader(),
+                Text("Enter your email address to reset your password",
+                    style: AppTextStyles.font12WhiteW600),
+                vGap(170),
+                CustomAuthTextFormField(
+                    hintText: "Email",
+                    suffixIcon: Icons.email_outlined,
+                    controller: emailController),
+                vGap(20),
+                CustomButton(
+                    text: "Send",
+                    onTap: () {
+                      if (formKey.currentState!.validate()) {
+                        context
+                            .read<SendPasswordResetCubit>()
+                            .sendPasswordReset(
+                                email: emailController.text.trim());
+                      }
+                    }),
+                const Spacer(),
+                vGap(40),
+                const BottomScreenActions(
+                  backExists: true,
+                  skipExists: false,
+                ),
+                vGap(20),
+                BlocListener<SendPasswordResetCubit, SendPasswordResetState>(
+                  listener: (context, state) {
+                    if (state is SendPasswordResetSuccess) {
+                      showToast(
+                          toastMsg:
+                              "Sent password reset to ${emailController.text.trim()}",
+                          state: ToastStates.success);
+                    } else if (state is SendPasswordResetFailure) {
+                      showToast(
+                          toastMsg: state.error, state: ToastStates.error);
+                    }
+                  },
+                  child: const SizedBox.shrink(),
+                )
+              ],
             ),
-            vGap(20)
-          ],
-        ),
-      ),
+          ),
+        );
+      }),
     );
   }
 }
