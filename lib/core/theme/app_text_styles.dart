e601d4af22bd2d0eea6d2ba7a945114ce44959e3
EslamElnady0 <eslamelnady254@gmail.com>
2024-09-28 03:08:05 +0300

    created lato text styles

--- lib/core/theme/app_text_styles.dart
+++ lib/core/theme/app_text_styles.dart
@@ -3,32 +3,51 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app_colors.dart';
 
+class LatoTextStyle extends TextStyle {
+  const LatoTextStyle({
+    super.fontSize,
+    super.fontWeight,
+    super.fontStyle,
+    super.color,
+    super.letterSpacing,
+    super.height,
+    super.decoration,
+  }) : super(
+          fontFamily: 'Lato',
+        );
+}
+
 class AppTextStyles {
   const AppTextStyles._();
 
   //naming ==> fontSizeColorWeight
-  static TextStyle font20DarkW600 = TextStyle(
+  static TextStyle font20DarkW600 = LatoTextStyle(
     fontSize: 20.sp,
     fontWeight: FontWeight.w600,
     color: AppColors.black,
   );
-  static TextStyle font15WhiteW500 = TextStyle(
+  static TextStyle font15WhiteW500 = LatoTextStyle(
     fontSize: 15.sp,
     fontWeight: FontWeight.w500,
     color: AppColors.white,
   );
-  static TextStyle font10WhiteW600 = TextStyle(
+  static TextStyle font10WhiteW600 = LatoTextStyle(
     fontSize: 10.sp,
     fontWeight: FontWeight.w600,
     color: AppColors.white,
   );
-  static TextStyle font19WhiteW600 = TextStyle(
+  static TextStyle font19WhiteW600 = LatoTextStyle(
     fontSize: 19.sp,
     fontWeight: FontWeight.w600,
     color: AppColors.white,
   );
-  static TextStyle font50WhiteW600 = TextStyle(
+  static TextStyle font50WhiteW600 = LatoTextStyle(
     fontSize: 50.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+  );
+  static TextStyle font26WhiteW600 = LatoTextStyle(
+    fontSize: 26.sp,
     fontWeight: FontWeight.w600,
     color: AppColors.white,
   );
