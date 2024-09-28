97baf7371ea29cc90491b5564b351a03890d6e21
EslamElnady0 <eslamelnady254@gmail.com>
2024-09-28 11:04:38 +0300

    handling login ano loading state

--- lib/features/auth/presentation/views/widgets/log_in_view_body.dart
+++ lib/features/auth/presentation/views/widgets/log_in_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:exo_planets/core/theme/app_colors.dart';
+import 'package:exo_planets/features/auth/presentation/view%20models/login%20ano%20cubit/login_ano_cubit.dart';
 import 'package:exo_planets/features/auth/presentation/views/widgets/custom_login_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../core/helpers/spacing.dart';
@@ -33,30 +36,44 @@ class _LogInViewBodyState extends State<LogInViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-                child: CustomLoginForm(
-                    formKey: formKey,
-                    emailController: emailController,
-                    passwordController: passwordController)),
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                children: [
-                  const Spacer(),
-                  vGap(40),
-                  const BottomScreenActions(
-                    backExists: true,
-                    skipExists: true,
+    return Stack(
+      children: [
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                    child: CustomLoginForm(
+                        formKey: formKey,
+                        emailController: emailController,
+                        passwordController: passwordController)),
+                SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Column(
+                    children: [
+                      const Spacer(),
+                      vGap(40),
+                      const BottomScreenActions(
+                        backExists: true,
+                        skipExists: true,
+                      ),
+                      vGap(20)
+                    ],
                   ),
-                  vGap(20)
-                ],
-              ),
-            )
-          ],
-        ));
+                )
+              ],
+            )),
+        BlocBuilder<LoginAnoCubit, LoginAnoState>(builder: (context, state) {
+          if (state is LoginAnoLoading) {
+            return Container(
+                color: AppColors.black.withOpacity(0.5),
+                child: const Center(
+                  child: CircularProgressIndicator(),
+                ));
+          }
+          return const SizedBox.shrink();
+        })
+      ],
+    );
   }
 }
--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../../core/helpers/spacing.dart';
+import '../../../../../core/theme/app_colors.dart';
+import '../../view models/login ano cubit/login_ano_cubit.dart';
 import 'bottom_screen_actions.dart';
 import 'custom_sign_up_form.dart';
 
@@ -42,32 +45,44 @@ class _SignUpViewBodyState extends State<SignUpViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-                child: CustomSignUpForm(
-                    formKey: formKey,
-                    emailController: emailController,
-                    passwordController: passwordController,
-                    firstNameController: firstNameController,
-                    lastNameController: lastNameController,
-                    confirmPasswordController: confirmPasswordController)),
-            SliverFillRemaining(
-                hasScrollBody: false,
-                child: Column(
-                  children: [
-                    const Spacer(),
-                    vGap(40),
-                    const BottomScreenActions(
-                      backExists: true,
-                      skipExists: true,
-                    ),
-                    vGap(20)
-                  ],
-                ))
-          ],
-        ));
+    return Stack(children: [
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(
+                  child: CustomSignUpForm(
+                      formKey: formKey,
+                      emailController: emailController,
+                      passwordController: passwordController,
+                      firstNameController: firstNameController,
+                      lastNameController: lastNameController,
+                      confirmPasswordController: confirmPasswordController)),
+              SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Column(
+                    children: [
+                      const Spacer(),
+                      vGap(40),
+                      const BottomScreenActions(
+                        backExists: true,
+                        skipExists: true,
+                      ),
+                      vGap(20)
+                    ],
+                  ))
+            ],
+          )),
+      BlocBuilder<LoginAnoCubit, LoginAnoState>(builder: (context, state) {
+        if (state is LoginAnoLoading) {
+          return Container(
+              color: AppColors.black.withOpacity(0.5),
+              child: const Center(
+                child: CircularProgressIndicator(),
+              ));
+        }
+        return const SizedBox.shrink();
+      })
+    ]);
   }
 }
