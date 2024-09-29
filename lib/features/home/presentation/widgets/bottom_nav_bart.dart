747187465f36a040422df5b8c66dc84cf5a4dc39
EslamElnady0 <eslamelnady254@gmail.com>
2024-09-29 22:13:08 +0300

    bottom nav bar refactor

--- lib/features/home/presentation/widgets/bottom_nav_bart.dart
+++ lib/features/home/presentation/widgets/bottom_nav_bart.dart
@@ -1,11 +1,10 @@
 import 'package:exo_planets/core/helpers/app_assets.dart';
 import 'package:exo_planets/core/theme/app_colors.dart';
-import 'package:exo_planets/core/widgets/inner_shadow.dart';
 import 'package:exo_planets/features/home/presentation/views/home_view.dart';
+import 'package:exo_planets/features/home/presentation/widgets/bottom_nav_bar_background.dart';
+import 'package:exo_planets/features/home/presentation/widgets/leaders_board_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
-
 import 'bottom_nav_bar_item.dart';
 
 class BottomNavBarController extends StatefulWidget {
@@ -36,91 +35,53 @@ class _BottomNavBarControllerState extends State<BottomNavBarController> {
     return Scaffold(
       backgroundColor: AppColors.black,
       extendBody: true,
-      bottomNavigationBar: InnerShadow(
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(16.0.r),
         child: SizedBox(
-          child: Padding(
-            padding: EdgeInsets.all(16.0.r),
-            child: SizedBox(
-              height: 72.0.h,
-              child: Stack(
-                clipBehavior: Clip.none,
-                children: [
-                  // bottom nav background
-                  InnerShadow(
-                    shadows: [
-                      BoxShadow(
-                        offset: const Offset(0, 0),
-                        blurRadius: 16,
-                        color: AppColors.black.withOpacity(0.25),
-                      ),
-                    ],
-                    child: SvgPicture.asset(
-                      width: double.infinity,
-                      AppAssets.bottomNavBackground,
-                    ),
-                  ),
+          height: 72.0.h,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              const BottomNavBarBackground(),
 
-                  // bottom nav items
-                  Positioned.directional(
-                    textDirection: TextDirection.ltr,
-                    top: 16.0.h,
-                    start: 0,
-                    end: 0,
-                    bottom: 0,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: List.generate(4, (rowIndex) {
-                          return Padding(
-                            padding: EdgeInsets.only(
-                                right: rowIndex == 1 ? 54.w : 0),
-                            child: InkWell(
-                              onTap: () {
-                                setState(() {
-                                  selectedIndex = rowIndex;
-                                  index = rowIndex;
-                                });
-                              },
-                              child: bottomNavBarItem(images[rowIndex],
-                                  labels[rowIndex], rowIndex, selectedIndex),
-                            ),
-                          );
-                        }),
-                      ),
-                    ),
-                  ),
-                  Positioned.directional(
-                    textDirection: TextDirection.ltr,
-                    start: 0,
-                    end: 0,
-                    bottom: 32.0.h,
-                    child: Align(
-                      child: InkWell(
-                        onTap: () {
-                          // routing to main here...
-                        },
-                        child: InnerShadow(
-                          shadows: [
-                            BoxShadow(
-                              offset: const Offset(0, 0),
-                              blurRadius: 5.3,
-                              color: AppColors.black.withOpacity(0.25),
-                            ),
-                          ],
-                          child: SvgPicture.asset(
-                            width: 50.0.w,
-                            height: 50.0.w,
-                            fit: BoxFit.fill,
-                            AppAssets.main,
-                          ),
+              // bottom nav items
+              Positioned.directional(
+                textDirection: TextDirection.ltr,
+                top: 16.0.h,
+                start: 0,
+                end: 0,
+                bottom: 0,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: List.generate(4, (rowIndex) {
+                      return Padding(
+                        padding:
+                            EdgeInsets.only(right: rowIndex == 1 ? 54.w : 0),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              selectedIndex = rowIndex;
+                              index = rowIndex;
+                            });
+                          },
+                          child: bottomNavBarItem(images[rowIndex],
+                              labels[rowIndex], rowIndex, selectedIndex),
                         ),
-                      ),
-                    ),
-                  )
-                ],
+                      );
+                    }),
+                  ),
+                ),
               ),
-            ),
+              Positioned.directional(
+                textDirection: TextDirection.ltr,
+                start: 0,
+                end: 0,
+                bottom: 32.0.h,
+                child: const LeadersBoardWidget(),
+              )
+            ],
           ),
         ),
       ),
